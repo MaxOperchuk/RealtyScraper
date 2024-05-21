fb77b10df0d7dc67770aa00caac72b8791f07366
Maksym Operchuk <operlast529@gmail.com>
2024-05-21 19:29:17 +0300

    Uncomment: DOWNLOADER_MIDDLEWARES const and change it to "scrapy_selenium.SeleniumMiddleware" in settings.py.

--- realty/settings.py
+++ realty/settings.py
@@ -50,9 +50,9 @@ COOKIES_ENABLED = True
 
 # Enable or disable downloader middlewares
 # See https://docs.scrapy.org/en/latest/topics/downloader-middleware.html
-#DOWNLOADER_MIDDLEWARES = {
-#    "realty.middlewares.RealtyDownloaderMiddleware": 543,
-#}
+DOWNLOADER_MIDDLEWARES = {
+   "scrapy_selenium.SeleniumMiddleware": 800,
+}
 
 # Enable or disable extensions
 # See https://docs.scrapy.org/en/latest/topics/extensions.html
