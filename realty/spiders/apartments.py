73fe300b3dcf11cb128df81b432251d39b531ec4
Maksym Operchuk <operlast529@gmail.com>
2024-05-21 19:22:51 +0300

    Create: start_requests method in ApartmentsSpider class.

--- realty/spiders/apartments.py
+++ realty/spiders/apartments.py
@@ -4,10 +4,23 @@ import scrapy
 class ApartmentsSpider(scrapy.Spider):
     name = "apartments"
     allowed_domains = ["realtylink.org"]
-    start_urls = ["https://realtylink.org/en/properties~for-rent"]
+
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.driver = webdriver.Chrome()
+
+    def start_requests(self) -> Iterable[Request]:
+        urls = []
+        start_url = "https://realtylink.org/en/properties~for-rent"
+
+        additional_urls = self._get_urls(start_url)
+        urls.extend(additional_urls)
+
+        for url in urls:
+
+            url = urljoin(start_url, url)
+
+            yield scrapy.Request(url=url, callback=self.parse)
 
 
     def parse(self, response):
