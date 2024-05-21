5e5870910dd05c1142f10f60a592b5b389cf221d
Maksym Operchuk <operlast529@gmail.com>
2024-05-21 19:21:37 +0300

    Rewrite: init method in ApartmentsSpider class.

--- realty/spiders/apartments.py
+++ realty/spiders/apartments.py
@@ -5,6 +5,10 @@ class ApartmentsSpider(scrapy.Spider):
     name = "apartments"
     allowed_domains = ["realtylink.org"]
     start_urls = ["https://realtylink.org/en/properties~for-rent"]
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.driver = webdriver.Chrome()
+
 
     def parse(self, response):
         pass
